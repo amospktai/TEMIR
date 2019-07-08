a2ba9926fc95fe527a55c386be6dc11bb9e001b8
Maiheliyaer SADEKE <mehliyar@gmail.com>
2019-07-08 16:49:27 +0800

    added a todo in r.s.dose

--- drydep_toolbox.R
+++ drydep_toolbox.R
@@ -22,7 +22,7 @@ f_Obuk = obk
 
 # Aerodynamic resistance (follows GEOS-Chem)
 r.a = function(ustar, z0, obk, T.k, cz) {
-   
+
    # Uses global constants:"von_Kar= 0.4" von Karman constant (unitless)"
    # Friction velocity "ustar" (m s^-1), roughness height "z0" (m), Monin-Obhukov Length "obk" (m), surface temperature "T.k" (K), reference height "cz" (m)
    # Calculate the kinematic viscosity xnu (m^2 s^-1) of air as a function of temperature
@@ -32,7 +32,7 @@ r.a = function(ustar, z0, obk, T.k, cz) {
    corr1 = cz/obk
    z0obk = z0/obk
    lrgera = FALSE # lrgera = T -> stable atmosphere; a high aerodynamic resistance (Ra = 1E4 m/s) is imposed, else Ra is calculated
-   
+
    if (reyno < 0.1) {
       r_a = 1E4 # aerodynamically rough or smooth surface
    } else {
@@ -47,9 +47,9 @@ r.a = function(ustar, z0, obk, T.k, cz) {
          r_a = (1/ckustar) * (log(corr1/z0obk) + 5 * (corr1 - z0obk))
       } else r_a = (1/ckustar) * (5 * log(corr1/z0obk) + (corr1 - z0obk))
    }
-   
+
    return(min(r_a, 1E4))
-   
+
 }
 
 f_aerodyn_cond = r.a
@@ -58,10 +58,10 @@ f_aerodyn_cond = r.a
 
 # Molecular diffusivity (m^2 s^-1)
 diffg = function(T.k, p, mx){
-   
+
    # Use global constants: "N_A = 6.02214e23" Avogadro constant (molecules mol^-1); "R_uni = 8.31446" universal gas constant (J K^-1 mol^-1); "M_da = 28.966e-3" Molar mass of dry air (kg mol^-1)
    # Surface temperature "T.k" (K), Pressure "p" (Pa), molecular weight "mx" (kg)
-   
+
    # Air density "rho" (molec m^-3)
    rho = p * N_A / R_uni / T.k
    # "d" is the collision diameter for gas X in air
@@ -74,23 +74,23 @@ diffg = function(T.k, p, mx){
    # Calculate diffusion coefficient of gas X in air
    diff.g = (3 * pi / 32) * (1 + z) * frpath * speed
    return(diff.g)
-   
+
 }
 
 ################################################################################
 
 # Boundary layer resistance
 r.b = function(T.k, p, mx, ustar){
-   
+
    # Uses external function "diff.g"
    # Uses global constants: "von_Kar= 0.4" von Karman constant (unitless)"
    # Temperature "T.k" (K), Pressure "p" (Pa), molecular weight "mx" (kg), friction velocity "ustar"  (m/s)
-   
+
    # dair is the thermal diffusivity of air; value of 0.2e-4 m^2 s^-1
    dair = 0.2e-4
    diff.g = diffg(T.k, p, mx)
    r_b = 2 / (von_Kar * ustar) * (dair / diff.g)^0.667
-   
+
    return(r_b)
 }
 
@@ -106,18 +106,18 @@ hstar.frac = function(hstar, f0){
 
 # Stomatal resistance  (follows GEOS-Chem v10.01, shsun)
 r.s.wesely.cld = function(rsmin, L, cosSZA, CLDFRAC, drydep.coef, T.c, par){
-   
+
    ## drydep_toolbox_mod.F90 & drydep_mod.F
    ## functions: BIOFIT, SUNPARAM_R4;SUNPARAM_R8
    ## BIOFIT_R4(drydep.coef, L, cosSZA, CLDFRAC, NPOLY) RESULT(BIO_FIT)
-   
+
    # cosSZA Cosine, solar zenith angle
    # CLDFRAC cloud fraction (unitless)
    # drydep.coef (NPOLY) Baldocchi polynomial drydep coefficients
    # L leaf area index
    # GFACI, resultant light correction
    # par solar radiation in W m-2
-   
+
    ### SUNPARAM
    nn = 3  ## number of variables (LAI, SUNCOS, CLDFRC)
    nd = c(55,20,11) ## scaling factor for each variable
@@ -126,12 +126,12 @@ r.s.wesely.cld = function(rsmin, L, cosSZA, CLDFRAC, drydep.coef, T.c, par){
    for (i in 1:nn) {
       X[i] = min(X[i], x0[i])
       ## XLOW = minimum for each variable
-      
+
       xlow = if (i != 3) x0[i]/nd[i] else 0
-      
+
       X[i] = max(X[i], xlow)/x0[i]
    }
-   
+
    ### BIOFIT_R4
    kk = 4
    term = c(1, L, cosSZA, CLDFRAC)
@@ -146,22 +146,22 @@ r.s.wesely.cld = function(rsmin, L, cosSZA, CLDFRAC, drydep.coef, T.c, par){
          }
       }
    }
-   
+
    bio_fit = 0
    for (k in seq_along(drydep.coef)) {
       bio_fit = bio_fit + drydep.coef[k]*realterm[k]
    }
    print(paste0('Are bio_fit and drydep.coef*realterm identical? ', identical(bio_fit, drydep.coef * realterm)), quote=FALSE)
    if (bio_fit <= 0.1) bio_fit = 0.1
-   
+
    ### drydep_mod.F
    if (rsmin < 9999) {
-      
+
       gfact = if (T.c > 0 & T.c < 40) 400/T.c/(40-T.c) else 100
       gfaci = if (par > 0 & L > 0) 1/bio_fit else 100
       rsmin = rsmin*gfaci*gfact
    }
-   
+
    return(rsmin)
 }
 
@@ -169,27 +169,27 @@ r.s.wesely.cld = function(rsmin, L, cosSZA, CLDFRAC, drydep.coef, T.c, par){
 ################################################################################
 
 # Stomatal resistance (Zhang et al. 2003)
-r.s.zhang = function(rsmin, brs, par.sun, par.sha, Lsun, Lsha, T.c, tmin, tmax, 
+r.s.zhang = function(rsmin, brs, par.sun, par.sha, Lsun, Lsha, T.c, tmin, tmax,
                      topt, vpd, bvpd, srad, psi.min, psi.max, use.betat, betat) {
-   
+
    # Minimum stomatal resistance "rsmin" (s m^-1), empirical light response constant for stomatal resistance "brs" (W m^-2)
    # Absorbed PAR averaged over sunlit and shaded canopy (per unit plant area): "phi_sun", "phi_sha" (W m^-2), sunlit and shaded plant area index: "Lsun", "Lsha" (m^2 m^-2)
    # surface temperature "T.c" (degC), minimum temperature "tmin"(degC), maximum temperature "tmax" (degC), optimum temperature "topt" (degC)
    # Vapour pressure deficit "vpd" (kPa^-1), water-vapour-pressure-deficit constant "bvpd" (kPa^-1)
    # "srad" = solar radiation (W m^-2), parameters specify leaf-water-potential dependency "psi.min" "psi.max" (MPa)
    # "use.betat = TRUE" specify to use TEMIR calculated water stress, water stress "betat", "is.wet = TRUE" canopy is wet
-   
+
    # Unstressed canopy stomatal conductance G.st
    g.par = Lsun / (rsmin * (1 + brs / par.sun)) + Lsha / (rsmin * (1 + brs / par.sha))
-   
+
    ## Temperature factor
    g.t = ((tmax - T.c) / (tmax - topt)) ^ ((tmax - topt) / (topt - tmin))
    g.t = g.t * (T.c - tmin) / (topt - tmin)
    g.t = max(g.t, 0.05)
-   
+
    ## Water-vapour-pressure deficit factor
    g.d = max(0.05, 1 - vpd * bvpd)
-   
+
    # Water stress (leaf water potential) factor
    if (use.betat) {
       g.w = betat
@@ -198,16 +198,16 @@ r.s.zhang = function(rsmin, brs, par.sun, par.sha, Lsun, Lsha, T.c, tmin, tmax,
       g.w = (psi - psi.min) / (psi.max - psi.min)
    }
    g.w = max(0.05, min(1, g.w))
-   
+
    # Stomatal resistance
    r_s = 1 / (g.par * g.t * g.d * g.w)
-   
+
    return(list(r_s = r_s, g_par = g.par, g_t = g.t, g_w = g.w, g_vpd = g.d))
-   
+
 }
 
 ################################################################################################################
-# DO3SE model Stomatal resistance calculation, initially implemented for POD3 calculation
+# DO3SE model stomatal resistance calculation, initially implemented for POD3 calculation
 # Sadiq 2019
 ################################################################################################################
 r.s.dose = function(g.max = 500, P_atm=P_atm, theta_atm=theta_atm, f.phen, T_min = 12, T_opt = 26, T_max = 40, T_v, f.min = 0.01, par_sun, par_sha, f_sun, light_alpha = 0.0105, VPD_min = 3.2, VPD_max = 1.2, vpd){
@@ -219,21 +219,22 @@ r.s.dose = function(g.max = 500, P_atm=P_atm, theta_atm=theta_atm, f.phen, T_min
   # f.T accounts for leaf temeprature
   # f.D accounts for vapour pressure deficit
   # f.SW accounts for soil water availability ### NOT AVAILABLE YET ###
-  
+
   # unit conversion
   mol_to_met = 1e-3*R_uni*theta_atm/P_atm
   g.max.met = g.max * mol_to_met
-  
+
   # f.phen is pre-calculated and read in
+  # todo: calculate it within this function
   f.phen = f.phen
-  
+
   # f.T = function of
   # Parameters: T_min, T_opt, T_max and f_min
   # Inputs from simulations: T (2-meter air temperarue, in K, convert unit to degC for this function)
   T_v_degC = T_v - 273.15
-  
+
   bt = (T_max - T_opt)/(T_opt - T_min)
-  
+
   # f.T calculation:
   if (T_v_degC < T_min) f.T = f.min
   else if (T_v_degC > T_max) f.T = f.min
@@ -244,33 +245,33 @@ r.s.dose = function(g.max = 500, P_atm=P_atm, theta_atm=theta_atm, f.phen, T_min
     bell_f2bt = bell_f2^bt
     f.T = max(f.min, (bell_f1*bell_f2bt))
   }
-  
+
   # f.light = function of
   # Parameters: light_alpha, f.min
   # Inputs from simulations: phi_sun, phi_sha, f_sun
-  
+
   f_sha = 1 - f_sun
   f_sun_par = 1 - exp(-1 * light_alpha * par_sun)
   f_sha_par = 1 - exp(-1 * light_alpha * par_sha)
-  f.light =  f_sun*f_sun_par + f_sha*f_sha_par 
-  
+  f.light =  f_sun*f_sun_par + f_sha*f_sha_par
+
   #f.D = function of
   #parameters: VPD_min, VPD_max,
   #Inputs: VPD
   f_tmp = (1 - f.min)*(VPD_min - vpd)/(VPD_min - VPD_max) + f.min
   f_tmp = max(f.min, f_tmp)
   f.D = min(f_tmp, 1)
-  
+
   #f.SW = function of
   #Parameters: SWP_min, SWP_max,
   #inputs: SWP
   # not implemented yet
   f.SW = 1
-  
+
   # Stomatal resistance
   f.TDS = max(f.min, f.T*f.D*f.SW)
   r_s = 1 / (g.max.met * f.phen * f.light * f.TDS)
-  
+
   return(list(r_s = r_s, f_phen = f.phen, f_light = f.light, f_tds = f.TDS, f_t = f.T,  f_d = f.D))
 }
 
@@ -288,7 +289,7 @@ r.cut.wesely = function(rcut0, T.c, L, rh, ustar){
 
 # Canopy cuticular resistance (Zhang et al. 2003, eq.9a, eq.9b)
 r.cut.zhang = function(rcut0, T.c, L, is.wet, rh, ustar){
-   
+
    # Base cuticular resistance "r.cut0" (s m^-1), surface temperature "T.c" (degC), Leaf area index "L" (m^2 m^-2), "is.wet = TRUE" canopy is wet
    # Relative humidity "rh", friction velocity "ustar"  (m/s)
    r.cut = if (is.wet) rcut0 * ustar^-1 * L^-0.5 else r.cut = rcut0 * exp(-0.03 * rh) * L^-0.25 * ustar^-1
@@ -311,7 +312,7 @@ r.dc = function(srad) {
 
 # In-canopy aerodynamic resistance (Zhang et al. eq.7 & eq.7a)
 r.ac = function(rac0_min, rac0_max, LAI, LAI_min, LAI_max, ustar) {
-   
+
    # leaf area index "LAI" (m^2 m^-2), friction velocity "ustar"  (m/s)
    rac0 = rac0_min + ((LAI - LAI_min)/(LAI_max - LAI_min))*(rac0_max - rac0_min)
    rac = (rac0*LAI^0.25)/(ustar^2)
@@ -322,7 +323,7 @@ r.ac = function(rac0_min, rac0_max, LAI, LAI_min, LAI_max, ustar) {
 
 # Lower canopy surface resistance
 rclx = function(hstar, f0, rcls, rclo){
-   
+
    # Henry's law constant "hstar", reactivity factor for oxidation of biological substances "f0"
    # lower canopy resistance for SO2 "rcls" (s m^-1), lower canopy resistance for ozone "rclo" (s m^-1)
    return((hstar / (1E5 * rcls) + f0 / rclo) ^ -1)
@@ -332,7 +333,7 @@ rclx = function(hstar, f0, rcls, rclo){
 
 # Ground surface resistance (GC)
 rgx = function(hstar, f0, rgs, rgo, T.c){
-   
+
    # Henry's law constant "hstar", reactivity factor for oxidation of biological substances "f0"; for O3, f0 = 1
    # ground resistance for SO2 "rgs" (s m^-1), ground resistance for ozone "rgo" (s m^-1)
    return((hstar / (1E5 * rgs) + f0 / rgo) ^ -1)
@@ -342,7 +343,7 @@ rgx = function(hstar, f0, rgs, rgo, T.c){
 
 # Use wesely resistance model to compute dry deposition velocity
 f_drydep_Wesely = function(rho = 0, T.s, H = 0, z0 = 0, cz = 0, rsmin = 0, r_s=NULL, use_temir_rs=FALSE, r_a=NULL, use_temir_ra=FALSE, drycoeff, cldfrac, cosSZA, L, p, mx, ustar, hstar, f0, h, SAI, srad, par, r.cut0, rcls, rclo, rgs, rgo, ra_g, e_a, co2_scale = FALSE) {
-   
+
    # Uses external functions "r.a", r.b", "r.s.wesely", "obk", "r.cut", "hstar.frac", "ra.dc", "rclx", "rgx"
    # Aerodynamic resistance "r_a" (s m^-1), "use_temir_ra = TRUE" specifies to use TEMIR calculated ra, air density "rho" (molec m^-3)
    # Surface temperature "T.s" (K), sensible heat flux "H" (W m^-2), roughness height "z0" (m), altitude "cz" (m)
@@ -354,12 +355,12 @@ f_drydep_Wesely = function(rho = 0, T.s, H = 0, z0 = 0, cz = 0, rsmin = 0, r_s=N
    # lower canopy resistance for O3 "rclo" (s m^-1), ground surface resistance "rgs" (s m^-1)
    # Base cuticular resistance "r.cut0" (s m^-1), lower canopy resistance for SO2 "rcls" (s m^-1), lower canopy resistance for ozone "rclo" (s m^-1), ground resistance for SO2 "rgs" (s m^-1),
    # Ground resistance for ozone "rgo" (s m^-1), ground aerodynamic resistance "ra_g" (s m^-1)
-   
+
    T.k = T.s
    T.c = T.k - 273.15
-   
+
    rh = max(0, min(1, e_a/f_esat(T.k))) * 100
-   
+
    # Raw stomatal resistance
    if (!use_temir_rs) {
       if ((L > 0) & (par > 0)){
@@ -370,9 +371,9 @@ f_drydep_Wesely = function(rho = 0, T.s, H = 0, z0 = 0, cz = 0, rsmin = 0, r_s=N
    } else {
       if (is.null(r_s)) stop('r_s has not been provided as input!') else r_s = min(r_s, 1E7)
    }
-   
+
    r_s = r_s * diffg(T.k, p, 18 * 10^-3) / diffg(T.k, p, mx)
-   
+
    # Raw cuticular resistance
    if (L > 0) {
       r_cut = r.cut.wesely(r.cut0, T.c, L, rh, ustar) / hstar.frac(hstar, f0)
@@ -380,11 +381,11 @@ f_drydep_Wesely = function(rho = 0, T.s, H = 0, z0 = 0, cz = 0, rsmin = 0, r_s=N
    } else {
       r_cut = 1E7 # if LAI is too low, shut down stomatal and cuticular uptake
    }
-   
+
    ra_dc = r.dc(srad) # in-canopy aerodynamic resistance
    rclx = rclx(hstar, f0, rcls, rclo) # lower canopy resistance (leaves, twig, bark, etc.)
    rgx = rgx(hstar, f0, rgs, rgo, T.c) # ground surface resistance (soil, leaf litter, etc.)
-   
+
    # aerodynamic resistance
    if (!use_temir_ra) {
       obk = obk(rho, T.s, ustar, H)
@@ -393,23 +394,23 @@ f_drydep_Wesely = function(rho = 0, T.s, H = 0, z0 = 0, cz = 0, rsmin = 0, r_s=N
       if (is.null(r_a)) stop('r_a has not been provided as input!')
       # Take r_a as provided.
    }
-   
+
    # quasi-laminar resistance
    r_b = r.b(T.k, p, mx, ustar)
-   
+
    ## Shsun (follows GC)
    g_c = 1/r_s + 1/r_cut + 1/(ra_dc + rclx) + 1/(rgx + ra_g)
    v_d = 1/(r_a + r_b + 1/g_c) #Calculate the dry deposition velocity
    out = list(r_a = r_a, r_b = r_b, r_s = r_s, r_cut = r_cut, ra_dc = ra_dc, rclx = rclx, ra_g = ra_g, rgx = rgx, v_d = v_d)
    return(out)
-   
+
 }
 
 ################################################################################
 
 # Use Zhang et al. (2003) model to comput ozone dry deposition velocity
 f_drydep_Zhang = function(rho = 0, T.s, H = 0, z0 = 0, cz =0, rsmin = 0, r_s=NULL, use_temir_rs=FALSE, r_a=NULL, use_temir_ra=FALSE, use_temir_beta, brs, par.sun, par.sha, Lsun, Lsha, tmin, tmax, topt, vpd, bvpd, e_a, srad, psi.min, psi.max, use.betat, betat, is.wet, L, LAI_min, LAI_max, rac0_min, rac0_max, p, mx, ustar, hstar, f0, h, SAI, r.cut0, rgs, rgo, fsnow, co2_scale = FALSE){
-   
+
    # Use external function "r.s.zhang", "r.a", "r.b", "diffg", "r.cut.zhang", "ra.dc", "rgx"
    # Aerodynamic resisatance "r_a", "use_temir_ra = TRUE" specifies to use TEMIR calculated ra, air density "rho" (molec m^-3),
    # Surface temperature "T.s" (K), sensible heat flux "H" (W m^-2), roughness height "z0" (m), altitude "cz" (m)
@@ -424,14 +425,14 @@ f_drydep_Zhang = function(rho = 0, T.s, H = 0, z0 = 0, cz =0, rsmin = 0, r_s=NUL
    # Henry's law constant "hstar", reactivity factor for oxidation of biological substances "f0"
    # Canopy height "h" (m), stem area index "SAI" (m^2 m^-2)
    # Base cuticular resistance "r.cut0" (s m^-1), ground resistance for SO2 "rgs" (s m^-1), ground resistance for ozone "rgo" (s m^-1), snow cover fraction (0.0-1.0) "fsnow"
-   
+
    T.k = T.s
    T.c = T.k - 273.15 # Kelvin to Celsius temperature
-   
+
    rh = max(0, min(1, e_a/f_esat(T.k))) * 100
-   
+
    ## leaf stomatal resistance
-   if (!use_temir_beta) betat = 1 
+   if (!use_temir_beta) betat = 1
    tmprs = r.s.zhang(rsmin, brs, par.sun, par.sha, Lsun, Lsha, T.c, tmin, tmax, topt, vpd, bvpd, srad, psi.min, psi.max, use.betat = use_temir_beta, betat)
    if (!use_temir_rs) {
       r_s = tmprs$r_s
@@ -440,24 +441,24 @@ f_drydep_Zhang = function(rho = 0, T.s, H = 0, z0 = 0, cz =0, rsmin = 0, r_s=NUL
       if (is.null(r_s)) stop('r_s has not been provided as input!')
       # Leave r_s as it is.
    }
-   
+
    g_par = tmprs$g_par ## Zhang et al. conductance reducing factors
    g_w = tmprs$g_w
    g_t = tmprs$g_t
    g_vpd = tmprs$g_vpd
-   
+
    r_s = r_s * diffg(T.k, p, 18E-3) / diffg(T.k, p, mx) # molecular diffusivities for water vapour and the pollutant gas
    r_s = min(r_s, 1E7)
-   
+
    ## Cuticle resistance
    r_cut = r.cut.zhang(r.cut0, T.c, L, is.wet, rh, ustar) / hstar.frac(hstar, f0)
-   
+
    ## In-canopy aerodynamic resistance
    ra_dc = r.ac(rac0_min, rac0_max, L, LAI_min, LAI_max, ustar)
-   
+
    ## Ground resistance for O3
    rgx = rgo
-   
+
    # Scale ground and cuticular resistance according to snow cover
    if (fsnow > 0) {
       rgs_snow = min(500, max(100, 70*(2-T.c)))
@@ -465,11 +466,11 @@ f_drydep_Zhang = function(rho = 0, T.s, H = 0, z0 = 0, cz =0, rsmin = 0, r_s=NUL
       r_cut = 1 / ((1 - fsnow) / r_cut + fsnow / r_snow)
       rgx = 1 / ((1 - min(2*fsnow, 1)) / rgx + min(2*fsnow, 1) / r_snow)
    }
-   
+
    # Prevent overestimation of r_cut for sparsely vegetated/snow-buried places
    LSAI = L + SAI
    r_cut = if (LSAI < 0.5) 1E7
-   
+
    ## Aerodynamic resistance
    if (!use_temir_ra) {
       obk = obk(rho, T.s, ustar, H)
@@ -478,10 +479,10 @@ f_drydep_Zhang = function(rho = 0, T.s, H = 0, z0 = 0, cz =0, rsmin = 0, r_s=NUL
       if (is.null(r_a)) stop('r_a has not been provided as input!')
       # Take r_a as provided.
    }
-   
+
    ## Sub-layer quasi-laminar resistance
    r_b = r.b(T.k, p, mx, ustar)
-   
+
    ## Stomatal blocking factor
    if (is.wet) { ## for wet canopies
       if (srad < 200) {
@@ -494,15 +495,15 @@ f_drydep_Zhang = function(rho = 0, T.s, H = 0, z0 = 0, cz =0, rsmin = 0, r_s=NUL
    } else { ## for dry canopies
       w.st = 0
    }
-   
+
    ### canopy conductance
    r_c = 1/((1-w.st)/r_s + 1/(ra_dc + rgx) + 1/r_cut)
-   
+
    ### dry deposition velocity
    v_d = 1/(r_a + r_b + r_c)
-   
+
    out = list(r_a = r_a, r_b = r_b, r_s = r_s, g_par = g_par, g_vpd = g_vpd, w_st = w.st, g_t = g_t, g_w = g_w, r_cut = r_cut, fsnow = fsnow, ra_dc = ra_dc, rgx = rgx, v_d = v_d)
-   
+
    return(out)
 }
 
